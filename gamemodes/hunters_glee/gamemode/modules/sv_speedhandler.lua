532d6ce869f47b21714eade5fbb3ce05c81fb58c
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2025-07-15 16:05:54 -0700

    Improve sv_speedhandler.lua comments

--- gamemodes/hunters_glee/gamemode/modules/sv_speedhandler.lua
+++ gamemodes/hunters_glee/gamemode/modules/sv_speedhandler.lua
@@ -1,34 +1,13 @@
--- three needed functionalities of this folder
-
--- add one function that lets you add/remove speed modifiers
--- remove speed modifiers by giving a speedKey with a nil speedModifier
--- add a speed modifier to a player by giving a speedKey with a speed modifer
-
--- use the self.glee_speedmodifiers to store speed modifiers
-
--- second function will allow the setting of a player's max speed
--- it will be another table of modifiers with the same structure as the first function
--- this function will add a "clamp" to the final result of all the added glee_speedmodifiers when the third function, refeshPlayerSpeed is ran
-
--- third and final function will be ran at the end of doSpeedModifier
--- it will take and store the player's current speed as self.glee_defaultspeed
--- then it will loop over all of the speed modifiers in the self.glee_speedmodifiers table, and add them to the default speed
-
--- so as an example i should be able to add two speed modifiers with the first function
--- "speedmod1" with a modifier of 10
--- "speedmod2" with a modifier of 40
--- and add a speed clamp with the second function
--- "speedclamp1" with a modifier of 40
-
--- the result of adding all of these modifers to one player means that the player has a movement speed of 40 above their normal speed
--- 10 + 50 clamped to 40
--- equals 40
-
--- GPT-4 result according to comments above ( i modified it a bit but it got close )
--- had to add isnumber check
 
 local plyMeta = FindMetaTable( "Player" )
 
+--[[--------------
+    ply:doSpeedModifier
+    Adds or removes a speed modifier for the player.
+    @param speedKey: A unique key for the speed modifier.
+    @param speedModifier: The speed modifier to apply. If nil, the modifier is removed.
+    @return: None
+--]]--------------
 function plyMeta:doSpeedModifier( speedKey, speedModifier )
     self.glee_speedmodifiers = self.glee_speedmodifiers or {}
 
@@ -44,6 +23,13 @@ function plyMeta:doSpeedModifier( speedKey, speedModifier )
 
 end
 
+--[[--------------
+    ply:doSpeedClamp
+    Adds or removes a speed clamp for the player.
+    @param speedKey: A unique key for the speed clamp.
+    @param speedClamp: The speed clamp to apply. If nil, the clamp is removed.
+    @return: None
+--]]--------------
 function plyMeta:doSpeedClamp( speedKey, speedClamp )
     self.glee_maxspeedmodifiers = self.glee_maxspeedmodifiers or {}
 
@@ -59,10 +45,16 @@ function plyMeta:doSpeedClamp( speedKey, speedClamp )
 
 end
 
+--[[--------------
+    ply:refeshPlayerSpeed
+    Refreshes the player's speed based on current modifiers and clamps.
+    @return: None
+--]]--------------
+
 function plyMeta:refeshPlayerSpeed()
     self.glee_speedmodifiers = self.glee_speedmodifiers or {}
     self.glee_maxspeedmodifiers = self.glee_maxspeedmodifiers or {}
-    self.glee_defaultspeed = self.glee_defaultspeed or self:GetRunSpeed()
+    self.glee_defaultspeed = self.glee_defaultspeed or self:GetRunSpeed() -- sets the default here!
 
     local walkSpeed = self:GetWalkSpeed()
     local defaultSpeed = self.glee_defaultspeed
