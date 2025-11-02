43f8f4127779b68d57bc5d04f6313e7f96a5b0c9
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2025-11-02 01:19:30 -0700

    fix

--- gamemodes/hunters_glee/gamemode/modules/cl_modelscale.lua
+++ gamemodes/hunters_glee/gamemode/modules/cl_modelscale.lua
@@ -4,9 +4,14 @@ net.Receive( "GLEE_PDM:UpdatePlyHull", function()
     local hullz = net.ReadUInt( 16 )
     local hullzduck = net.ReadUInt( 16 )
 
-    local ply = LocalPlayer()
-    if not IsValid( ply ) then print( "AAAAAAAAAAAAAAAAA" ) return end
-    ply:SetHull( Vector( -hull, -hull, 0 ), Vector( hull, hull, hullz ) )
-    ply:SetHullDuck( Vector( -hull, -hull, 0 ), Vector( hull, hull, hullzduck ) )
+    timer.Create( "GLEE_PDM:DelayedHullUpdate", 0, 0, function()
+        local ply = LocalPlayer()
+        if not IsValid( ply ) then return end
 
+        ply:SetHull( Vector( -hull, -hull, 0 ), Vector( hull, hull, hullz ) )
+        ply:SetHullDuck( Vector( -hull, -hull, 0 ), Vector( hull, hull, hullzduck ) )
+
+        timer.Remove( "GLEE_PDM:DelayedHullUpdate" )
+
+    end )
 end )
