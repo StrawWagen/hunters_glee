b7737588af741f99f193b458fe93eaff7a8bcbf9
Boomertaters <boomeritaintaters@gmail.com>
2025-11-26 11:05:45 -0500

    add new instruction

--- lua/weapons/termhunt_ar3.lua
+++ lua/weapons/termhunt_ar3.lua
@@ -3,7 +3,11 @@
 
 SWEP.PrintName = "Emplacement Gun"
 SWEP.Author = "Boomeritaintaters + gaming98"
-SWEP.Instructions = "I want revenge. I want them to know that death is coming - John Rambo"
+SWEP.Instructions = [[
+The longer you shoot, the faster the bullets fly. Just don't let it overheat!
+
+“I want revenge. I want them to know that death is coming. And there's nothing they can do to stop it.”
+ - John Rambo]]
 SWEP.Category = "Hunter's Glee"
 
 SWEP.Spawnable = true
@@ -283,4 +287,5 @@ function SWEP:DrawWorldModel()
     stopHeatDrawing( self, self )
 
 end
+
 
