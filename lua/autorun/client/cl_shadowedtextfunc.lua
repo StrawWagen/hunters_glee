dee3ff2a7c55ab1ef61c0623659d4e0f10be4fc8
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2026-05-09 17:01:34 -0500

    Simplify surface.drawShadowedTextBetter()

--- lua/autorun/client/cl_shadowedtextfunc.lua
+++ lua/autorun/client/cl_shadowedtextfunc.lua
@@ -3,16 +3,8 @@ AddCSLuaFile()
 
 if not CLIENT then return end
 
-local color_black = Color( 0, 0, 0 )
-
 local string_Explode = string.Explode
 local ipairs = ipairs
-local CurTime = CurTime
-local table_Count = table.Count
-local math_abs = math.abs
-
-local cachedShadowColors = {}
-local cachedShadowColorsLastUse = {}
 
 local surface_SetFont = surface.SetFont
 local surface_GetTextSize = surface.GetTextSize
@@ -42,16 +34,7 @@ function surface.drawShadowedTextBetter( textInitial, font, color, posx, posy, d
 
         end
 
-        local cacheKey = font .. tostring( color )
-        local shadowColor = cachedShadowColors[cacheKey]
-        cachedShadowColorsLastUse[cacheKey] = CurTime()
-        if not shadowColor then
-            shadowColor = ColorAlpha( color_black, shadowAlpha )
-            cachedShadowColors[cacheKey] = shadowColor
-
-        end
-
-        surface_SetTextColor( shadowColor )
+        surface_SetTextColor( 0, 0, 0, shadowAlpha )
         surface_SetTextPos( posx + centeringOffset + 2.5, posy + 2 + totalHeight )
         surface_DrawText( text )
 
@@ -63,22 +46,3 @@ function surface.drawShadowedTextBetter( textInitial, font, color, posx, posy, d
 
     end
 end
-
-local oldCount = 0
-
-timer.Create( "glee_shadowedfunc_cleanupcolors", 1, 0, function()
-    local newCount = table_Count( cachedShadowColors )
-    if newCount == oldCount then return end
-    oldCount = newCount
-
-    local cur = CurTime()
-
-    for cacheKey, _ in pairs( cachedShadowColors ) do
-        local lastUsed = cachedShadowColorsLastUse[cacheKey]
-        if math_abs( cur - lastUsed ) > 1 then
-            cachedShadowColorsLastUse[cacheKey] = nil
-            cachedShadowColors[cacheKey] = nil
-
-        end
-    end
-end )
