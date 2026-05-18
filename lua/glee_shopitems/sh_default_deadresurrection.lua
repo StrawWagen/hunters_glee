40facfde10e6585fe63bd6cc4dd2819effb14fa5
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-05-17 17:24:49 -0700

    Second chances

--- lua/glee_shopitems/sh_default_deadresurrection.lua
+++ lua/glee_shopitems/sh_default_deadresurrection.lua
@@ -567,7 +567,8 @@ if SERVER then
             end )
 
             -- patience system - global timer, but we track if we created it
-            GAMEMODE.roundExtraData.divinePatienceEnds = GAMEMODE.roundExtraData.divinePatienceEnds or ( CurTime() + 90 )
+            -- always reset patience for a newly chosen player, even if a previous one already expired it
+            GAMEMODE.roundExtraData.divinePatienceEnds = CurTime() + 90
 
             -- increase patience on player deaths (global hook, only create once)
             if not GAMEMODE.roundExtraData.createdThePatienceIncreaseHook then
