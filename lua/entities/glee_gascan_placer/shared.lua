98fed62d0327180065317054cf118fec2888acfa
Boomertaters <boomeritaintaters@gmail.com>
2026-05-27 17:30:23 -0400

    thanks claude

--- lua/entities/glee_gascan_placer/shared.lua
+++ lua/entities/glee_gascan_placer/shared.lua
@@ -39,7 +39,6 @@ if not SERVER then return end
 function ENT:ManageMyPos()
     BaseClass.ManageMyPos( self )
 
-    -- random angle each time based on entity index
     local ang = Angle( 90, ( self:EntIndex() * self:EntIndex() ) % 360, 0 )
     self:SetAngles( ang )
 
